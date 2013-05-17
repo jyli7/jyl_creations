b283652c4bad32e89bf7bc86f672eb9c09fc3901
jimmyli <jyl702@gmail.com>
2013-05-16 20:12:30 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JylCreations::Application.config.secret_token = '5ebe3fed4b3a6e3fef6ee6c4eba9f8f36275a084262c70604802a908450dfd6f1ea9c33fded4ff4f3c79011d0278579dc9987e9527521464d0a13743858ae79e'
