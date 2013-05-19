51fa457ab982e45d97f8a27f4443da8a07da47bc
jimmyli <jyl702@gmail.com>
2013-05-19 16:00:37 -0400

    nicer front

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
   def index
     @items = [
-      {name: "Apply Up", link: "applyup.herokuapp.com", image: "apply_up.png"},
-      {name: "YLS Class Action", link: "www.ylsclassaction.com", image: "yls_class_action.png"},
-      {name: "Year after Year", link: "www.yearafteryear.org", image: "year_after_year.png"},
-      {name: "Grounch", link: "www.groun.ch", image: "grounch.png"},
-      {name: "Lake Chelan", link: "lake-chelan.herokuapp.com", image: "lake_chelan.png"}
+      {name: "Apply Up", link: "http://applyup.herokuapp.com", image: "apply_up.png"},
+      {name: "YLS Class Action", link: "http://www.ylsclassaction.com", image: "yls_class_action.png"},
+      {name: "Year after Year", link: "http://www.yearafteryear.org", image: "year_after_year.png"},
+      {name: "Grounch", link: "http://www.groun.ch", image: "grounch.png"},
+      {name: "Lake Chelan", link: "http://lake-chelan.herokuapp.com", image: "lake_chelan.png"}
     ]
 
   end
