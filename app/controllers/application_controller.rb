b500db5994aa0d1e4cca9864d8c10cf3d0110108
jimmyli <jyl702@gmail.com>
2013-05-22 16:59:18 -0400

    auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :authenticate
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
+  end
+
+  def authenticate
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == "jyl702" && password == "cacpassfoo7"
+      end
+    end
   end
 
 end
