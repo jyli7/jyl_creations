47b68f9df9472d85c612d76539421ceaa54f3d8f
jimmyli <jyl702@gmail.com>
2013-07-10 18:48:12 -0400

    get filters working

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -1,6 +1,12 @@
 $(function () {
-	$('.work').isotope({
-		itemSelecter: '.work-item-container',
+	$('.work-wrapper').isotope({
+		itemSelector: '.work-item-container',
 		layoutMoude: 'fitRows'
+	});
+
+	$('.work-filters a').on('click', function (e) {
+		e.preventDefault();
+		var selector = $(this).data('filter');
+		$('.work-wrapper').isotope({ filter: '.' + selector });
 	});
 });
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,26 @@ class HomeController < ApplicationController
   def index
     @items = [
       {
+        name: "Canyon Racer",
+        link: "http://www.playcanyonracer.com",
+        image: "yls_class_action.png",
+        type: "game",
+        description: "3 Levels of Canyon Racing Fun",
+        tech: "[Javascript, Node.js]"
+      },
+      {
+        name: "Lattice",
+        link: "http://www.github.com/jyli7/lattice",
+        image: "yls_class_action.png",
+        type: "tool",
+        description: "Lightweight framework for making games in Javascript",
+        tech: "[Javascript]"
+      },
+      {
         name: "YLS Class Action",
         link: "http://www.ylsclassaction.com",
         image: "yls_class_action.png",
+        type: "web",
         description: "Better course selection for Yale Law School's 600 students.",
         tech: "[Rails, Javascript, jQuery, Postgresql]"
       },
@@ -12,6 +29,7 @@ class HomeController < ApplicationController
         name: "Year after Year",
         link: "http://www.yearafteryear.org",
         image: "year_after_year.png",
+        type: "web",
         description: "Pledge to give to the causes you care about, year after year.",
         tech: "[Rails, Javascript, jQuery, MongoDB]"
       },
@@ -19,6 +37,7 @@ class HomeController < ApplicationController
         name: "Pivotal Labs",
         link: "http://www.pivotallabs.com",
         image: "pivotal_labs.png",
+        type: "web",
         description: "Software Engineer. Used agile methodologies and rigorous test-driven development to
         develop several projects (e.g. a complete RESTful API for a mobile client,",
         tech: "[Ruby, Rails, Javascript, MySQL]"
@@ -27,6 +46,7 @@ class HomeController < ApplicationController
         name: "Codecademy",
         link: "http://www.codecademy.com",
         image: "codecademy.png",
+        type: "web",
         description: "Full stack developer. Worked with other developers to design and implement products across
         the site (groups, quizzes, user dashboard, user orientation, activity feed)",
         tech: "[Rails, Javascript, MongoDB, Redis]"
@@ -34,7 +54,8 @@ class HomeController < ApplicationController
       {
         name: "Apply Up",
         link: "http://applyup.herokuapp.com",
-        image: "apply_up.png", 
+        image: "apply_up.png",
+        type: "web",
         description: "Rails project for client. (Work in progress, password protected).",
         tech: "[Rails, Javascript, Backbone, MongoDB]"
       }
